bc2283e58527bf054d5179d0ff028726d1df608a
mohansanjay <sahumohan630@gmail.com>
2021-11-09 19:16:51 +0530

    adding position of strings program

--- hello.py
+++ hello.py
@@ -1,17 +1,6 @@
-print("helloo")
-print("i got to pushed it on github")
-# description about the loops
-while True:
-    'stmt'
-    'stmt'
-    break
-for p in range(1,6):
-    print("hello")
-    if p==3:
-        print("RRR")
-        continue
-print("can you pleased guess what happend with continue")
-
-
-
-
+inp="aaaabbbccdddeeddsaj"
+inp=list(inp)
+print(inp)
+d={}.fromkeys(inp,0)
+c=1
+print("unique Values",d)
